fb34b417ce1ab328a9fb4c0cf71c0bcb25263679
Axure <axurez@vip.qq.com>
2015-05-02 23:51:47 +0800

    Finall fixed the bug. That the title is also unique in matching.

--- D_crawler.php
+++ D_crawler.php
@@ -15,10 +15,10 @@ $main_content = file_get_contents($url);
 //print_r($main_content);
 $test_content = '<a href="http://www.feeyo.com/vflight/delay/a/CSX_SZX_ZH9978.htm" target="_blank" title="CSX-SZX">ZH9978</a>';
 
-$uri_pattern = "/<a href='(.*?)' target='_blank' title='CSX-SZX'>(.*?)<\/a>/";
+$uri_pattern = "/<a href='(.*?)' target='_blank' title='(.*?)'>(.*?)<\/a>/";
 
 //$uri_pattern = '/a/';
 preg_match_all($uri_pattern, $main_content, $matches);
 //preg_match_all($uri_pattern, $test_content, $matches);
-print_r($matches[0][0]);
+print_r($matches[0]);
 //print($main_content);

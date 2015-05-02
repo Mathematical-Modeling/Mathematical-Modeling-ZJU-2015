bf7096c199ae2e2a6c3ba3386da738b92d5e5c3a
Axure <axurez@vip.qq.com>
2015-05-03 00:00:00 +0800

    Start to implement a function to get the details of each flight. Implemented the title.

--- D_crawler.php
+++ D_crawler.php
@@ -6,6 +6,14 @@
  * Time: 23:21
  */
 
+function get_detail($detail_url, $detail_name) {
+    $detail_content = file_get_contents($detail_url);
+    $detail_title_pattern = '/<strong>' . $detail_name . ' (.*?)<\/strong>/';
+    preg_match_all($detail_title_pattern, $detail_content, $detail_title);
+    print_r($detail_title);
+
+}
+
 
 $url = "http://www.feeyo.com/vflight/flightlist.htm";
 
@@ -20,5 +28,9 @@ $uri_pattern = "/<a href='(.*?)' target='_blank' title='(.*?)'>(.*?)<\/a>/";
 //$uri_pattern = '/a/';
 preg_match_all($uri_pattern, $main_content, $matches);
 //preg_match_all($uri_pattern, $test_content, $matches);
-print_r($matches[0]);
+//print_r($matches[0]);
 //print($main_content);
+
+$detail_url = 'http://www.feeyo.com/vflight/delay/a/HGH_CDG_ZI1907.htm';
+$detail_name = 'ZI1907';
+get_detail($detail_url, $detail_name);

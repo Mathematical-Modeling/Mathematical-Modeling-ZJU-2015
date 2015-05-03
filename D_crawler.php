34d3b51a7f9ec613eaddd3a95a743d15084506a2
Axure <axurez@vip.qq.com>
2015-05-03 20:46:45 +0800

    A final version that is working (except the delay may still be too small so that I got around 5 misses.

--- D_crawler.php
+++ D_crawler.php
@@ -1,3 +1,5 @@
+<meta charset="GBK">
+
 <?php
 /**
  * Created by PhpStorm.
@@ -10,16 +12,20 @@ function get_detail($detail_url, $detail_name) {
     $detail_content = file_get_contents($detail_url);
     $detail_title_pattern = '/<strong>' . $detail_name . ' (.*?)<\/strong>/';
     preg_match_all($detail_title_pattern, $detail_content, $detail_title);
-    print_r($detail_title);
+//    print_r($detail_title);
+
 
     $detail_content = file_get_contents($detail_url);
     $detail_right_pattern = '/<td>(.*)<\/td>/';
 //    $detail_right_pattern = '/<strong>' . $detail_name . ' (.*?)<\/strong>/';
     preg_match_all($detail_right_pattern, $detail_content, $detail_right);
-    print_r($detail_right[1][2]);
-    print_r($detail_right[1][3]);
-    print_r($detail_right[1][4]);
-    print_r($detail_right[1][5]);
+//    print_r($detail_right[1][2]);
+//    print_r($detail_right[1][3]);
+//    print_r($detail_right[1][4]);
+//    print_r($detail_right[1][5]);
+
+    print($detail_name . ',' . $detail_title[1][0] . ',' .$detail_right[1][2] . ','.$detail_right[1][3] . ','.$detail_right[1][4] . ','.$detail_right[1][5] . "\n");
+    sleep(1);
 
 }
 
@@ -37,13 +43,21 @@ $uri_pattern = "/<a href='(.*?)' target='_blank' title='(.*?)'>(.*?)<\/a>/";
 //$uri_pattern = '/a/';
 preg_match_all($uri_pattern, $main_content, $matches);
 //preg_match_all($uri_pattern, $test_content, $matches);
-print_r($matches[1]);
+//print_r($matches[1]);
 //print($main_content);
 
-foreach($matches[1] as $url) {
-    get_detail($url, 'fds');
+//$new_matches = [];
+//for ($i = 0; $i < 5; ++$i) {
+//    $new_matches[] = $matches[1][$i];
+//}
+//print_r($matches[2]);
+
+print_r("ID,Name,Right,Half,One,More\n");
+
+foreach($matches[1] as $key => $value) {
+    get_detail($value, $matches[3][$key]);
 }
 
-$detail_url = 'http://www.feeyo.com/vflight/delay/a/HGH_CDG_ZI1907.htm';
-$detail_name = 'ZI1907';
-get_detail($detail_url, $detail_name);
+//$detail_url = 'http://www.feeyo.com/vflight/delay/a/HGH_CDG_ZI1907.htm';
+//$detail_name = 'ZI1907';
+//get_detail($detail_url, $detail_name);
